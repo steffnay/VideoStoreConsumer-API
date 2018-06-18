8acd985647ca3ba60336015015cd36f47d948aaf
Steffany Brown <steffannbrown@gmail.com>
2018-06-18 15:02:50 -0700

    updated movie_wrapper

--- lib/movie_wrapper.rb
+++ lib/movie_wrapper.rb
@@ -1,6 +1,6 @@
 class MovieWrapper
   BASE_URL = "https://api.themoviedb.org/3/"
-  KEY = ENV["MOVIEDB_KEY"]
+  KEY = ENV["TMDB_KEY"]
 
   BASE_IMG_URL = "https://image.tmdb.org/t/p/"
   DEFAULT_IMG_SIZE = "w185"
