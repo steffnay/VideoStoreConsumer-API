3b47825d17379b564b4fcf1d7614ec19c5b92978
Steffany Brown <steffannbrown@gmail.com>
2018-06-19 09:39:04 -0700

    added rack-cors gem. make sure to do bundle install

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,11 @@ module VideoStoreAPIRails
     #this loads everything in the lib folder automatically
     config.eager_load_paths << Rails.root.join('lib')
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
