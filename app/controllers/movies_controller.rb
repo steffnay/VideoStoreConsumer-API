0023c09e7a62167e51a7d67afee510c68956937f
Steffany Brown <steffannbrown@gmail.com>
2018-06-18 19:11:41 -0700

    added movies post route and controller create method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,12 +21,36 @@ class MoviesController < ApplicationController
       )
   end
 
+  def create
+    new_movie = Movie.new(
+      title: movie_params[:title],
+      overview: movie_params[:overview],
+      release_date: movie_params[:release_date],
+      image_url: movie_params[:image_url],
+      external_id: movie_params[:external_id])
+
+
+    if new_movie.save
+      render json: { external_id: new_movie.external_id }, status: :ok
+      return
+    else
+      render json: {ok: false, errors: "Movie not added to inventory"}, status: :bad_request
+      return
+    end
+
+  end
+
   private
 
   def require_movie
-    @movie = Movie.find_by(title: params[:title])
+    @movie = Movie.where('lower(title) = ?', params[:title].downcase).first
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
   end
+
+  def movie_params
+    params.permit(:title, :overview, :release_date, :image_url, :external_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :movies, only: [:index, :show], param: :title
 
+  post '/movies', to: 'movies#create', as: 'add_movie'
+
   post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
   get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
