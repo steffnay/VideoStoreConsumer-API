7bfa770971bb96a37478900a90fb1c34d281cd68
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-06-19 10:34:47 -0700

    working rental component

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,6 +2,21 @@ class RentalsController < ApplicationController
   before_action :require_movie, only: [:check_out, :check_in]
   before_action :require_customer, only: [:check_out, :check_in]
 
+  # Added by us!
+  def index
+    rentals = Rental.all.map do |rental|
+      {
+          title: rental.movie.title,
+          customer_id: rental.customer_id,
+          name: rental.customer.name,
+          postal_code: rental.customer.postal_code,
+          checkout_date: rental.checkout_date,
+          due_date: rental.due_date
+      }
+    end
+    render status: :ok, json: rentals
+  end
+
   # TODO: make sure that wave 2 works all the way
   def check_out
     rental = Rental.new(movie: @movie, customer: @customer, due_date: params[:due_date])
